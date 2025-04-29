7e370ecfcfb9e8b2d73d3d2b82ff364e715f5b27
Kipchumba Bett <kipchubett@gmail.com>
2025-04-29 15:37:19 +0300

    Use `@mekomsolutions/esm-patient-tests-app` version `9.2.3-pre.1000` (#78)

---
+++ scripts/openmrs/frontend_assembly/build-openmrs-frontend.groovy
@@ -0,0 +1,88 @@
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+import java.nio.file.Paths
+
+log.info("Checking for frontend customizations in ${Paths.get("${project.build.directory}", "openmrs_frontend", "spa-assemble-config.json").toString()}")
+
+def refAppConfigFile = Paths.get("${project.build.directory}", "openmrs_frontend", "reference-application-spa-assemble-config.json").toFile()
+
+def slurper = new JsonSlurper()
+def refAppConfig = slurper.parse(refAppConfigFile)
+
+frontendCustomizationsFile = Paths.get("${project.build.directory}", "openmrs_frontend", "spa-assemble-config.json").toFile()
+if (frontendCustomizationsFile.exists()) {
+    frontendCustomizationsConfig = slurper.parse(frontendCustomizationsFile)
+    // Get excluded frontend modules from the customizations file
+    def excludedFrontendModules = frontendCustomizationsConfig["excludedFrontendModules"] ?: []
+
+    // Remove excluded modules from reference application config
+    excludedFrontendModules.each { module ->
+        if (refAppConfig.frontendModules?.containsKey(module)) {
+            log.info("Removing '${module}' from the reference application configuration...")
+            refAppConfig.frontendModules.remove(module)
+            refAppConfigFile.text = JsonOutput.prettyPrint(JsonOutput.toJson(refAppConfig))
+            log.info("Removed '${module}' from the configuration.")
+        }
+    }
+}
+
+def openmrsVersion = refAppConfig["coreVersion"] ?: "next"
+def outputDirectory = "${project.groupId}" == "com.ozonehis" && "${project.artifactId}" == "ozone-distro" ?
+        "${project.build.directory}/${project.artifactId}-${project.version}/binaries/openmrs/frontend" :
+        "${project.build.directory}/${project.artifactId}-${project.version}/distro/binaries/openmrs/frontend"
+
+def outputDirectoryFile = new File(outputDirectory)
+
+def assembleCommand = "npx --legacy-peer-deps openmrs@${openmrsVersion} assemble --manifest --mode config --target ${outputDirectory} --config ${refAppConfigFile.getAbsolutePath()}"
+log.info("Project: ${project.groupId}:${project.artifactId}")
+// by default, we build the frontend as part of Ozone and only rebuild if there are local customizations
+def shouldBuildFrontend = "${project.groupId}" == "com.ozonehis" && "${project.artifactId}" == "ozone-distro"
+
+if (!shouldBuildFrontend) {
+    if (frontendCustomizationsFile.exists()) {
+        log.info("Found frontend customizations. Rebuilding frontend...")
+
+        assembleCommand += " --config ${frontendCustomizationsFile.getAbsolutePath()}"
+        shouldBuildFrontend = true
+        // Update the OpenMRS version to the one specified in the customizations file if it exists.
+        def newOpenmrsCoreVersion = slurper.parse(frontendCustomizationsFile)["coreVersion"] ?: openmrsVersion
+        // Update the OpenMRS version in the assemble command if it is different from the current version.
+        if (newOpenmrsCoreVersion != openmrsVersion) {
+            assembleCommand = assembleCommand.replace(openmrsVersion, newOpenmrsCoreVersion)
+            openmrsVersion = newOpenmrsCoreVersion
+        }
+        log.info("Using OpenMRS Frontend Core Version: ${openmrsVersion}")
+    }
+}
+
+if (shouldBuildFrontend) {
+    log.info("Cleaning ${outputDirectory}...")
+    if (outputDirectoryFile.exists()) {
+        outputDirectoryFile.eachFile(it -> it.delete())
+        outputDirectoryFile.eachDir(it -> { if (it.getName() != "ozone") { it.deleteDir() } })
+    }
+
+    log.info("Running assemble command...")
+    log.info(assembleCommand)
+
+    def assembleProcess = assembleCommand.execute()
+    assembleProcess.consumeProcessOutput(System.out, System.err)
+    assembleProcess.waitFor()
+
+    if (assembleProcess.exitValue() != 0) {
+        throw new RuntimeException("'openmrs assemble' step failed. See previous messages for details.")
+    }
+
+    log.info("Running build command...")
+
+    def buildProcess = "npx --legacy-peer-deps openmrs@${openmrsVersion} build --config-url \$SPA_CONFIG_URLS --default-locale \$SPA_DEFAULT_LOCALE --target ${outputDirectory}".execute()
+    buildProcess.consumeProcessOutput(System.out, System.err)
+    buildProcess.waitFor()
+
+    if (buildProcess.exitValue() != 0) {
+        throw new RuntimeException("'openmrs build' step failed. See previous messages for details.")
+    }
+} else {
+    log.info("No need to re-build the frontend detected")
+}
